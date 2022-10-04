4035f606f47b085f580ac2e7726017f2389f227a
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-10-04 04:31:03 -0700

    Create Query_Weighted_Average_Calc.sql

---
+++ Gyakorlatok/Neptun/Query_Weighted_Average_Calc.sql
@@ -0,0 +1,10 @@
+select h.neptun, vnev, knev,
+AVG(Cast(jegy as float) * kredit)/ SUM(kredit)
+from hallgatok h, jegyek j, tantargyak t
+where h.neptun = j.neptun and j.tkod = t.kod
+and vdatum = (Select MAX(vdatum) from jegyek j2 /*Alselect*/
+where j2.neptun = j.neptun and j2.tkod = j.tkod)
+group by h.neptun, vnev, knev /*Amit a selectbe beírunk a group by-ba is be kell írni*/
+order by 4 DESC /*4. oszlop alapján rendezi csökkenőben, DESC nélkül növekvőben*/
+
+/*Ez valamiért nem működik!!!*/
