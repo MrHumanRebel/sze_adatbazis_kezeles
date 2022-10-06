035d144bfae810306c5b3473eeeb5ac7c953bc00
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-10-06 07:57:17 -0700

    add f1 scripts

---
+++ formula_1/create_f1_tables.sql
@@ -0,0 +1,93 @@
+USE Formula_1
+GO
+
+-- CREATE TABLE versenyzok
+CREATE TABLE Formula_1.dbo.versenyzok
+(
+ versenyzo_id char(2) NOT NULL,
+ keresztnev nvarchar(50) NOT NULL,
+ vezeteknev nvarchar(50) NOT NULL,
+ orszag nvarchar(56) NOT NULL,
+ beszelt_nyelv nvarchar(50) NOT NULL,
+ konstruktor_id char(2) NOT NULL,
+ CONSTRAINT PK_versenyzok PRIMARY KEY (versenyzo_id ASC)
+ )
+GO
+
+-- CREATE TABLE konstruktorok
+CREATE TABLE Formula_1.dbo.konstruktorok
+(
+ konstruktor_id char(2) NOT NULL
+ nev nvarchar(50) NOT NULL
+ csapatvezeto nvarchar(70) NOT NULL
+ orszag nvarchar(56) NOT NULL
+ CONSTRAINT PK_konstruktorok PRIMARY KEY (konstruktor_id ASC)
+ )
+GO
+
+-- CREATE TABLE futamok
+CREATE TABLE Formula_1.dbo.futamok(
+futam_id char(2) NOT NULL
+szezon_id char(2) NOT NULL
+helyszin_id char(2) NOT NULL
+datum date NOT NULL
+ido time(7( NOT NULL
+idojarasi_korulmeny_id char(2) NOT NULL
+CONSTRAINT PK_futamok PRIMARY KEY (futam_id ASC)
+
+ )
+GO
+
+-- CREATE TABLE helyszin
+CREATE TABLE Formula_1.dbo.helyszin(
+helyszin_id char(2) NOT NULL
+palyanev nvarchar(50) NOT NULL
+telepules nvarchar(50) NOT NULL
+orszag nvarchar(56) NOT NULL
+CONSTRAINT PK_helyszin PRIMARY KEY (helyszin_id ASC)
+
+ )
+GO
+
+-- CREATE TABLE idojarasi_korulmenyek
+CREATE TABLE Formula_1.dbo.idojarasi_korulmenyek(
+idojarasi_korulmeny_id char(2) NOT NULL
+nev nvarchar(50) NOT NULL
+CONSTRAINT PK_idojarasi_korulmenyek PRIMARY KEY (idojarasi_korulmeny_id ASC)
+
+ )
+GO
+
+-- CREATE TABLE helyezesek
+CREATE TABLE Formula_1.dbo.helyezesek(
+versenyzo_id char(2) NOT NULL
+elert_helyezes char(2) NOT NULL
+pole_poz bit
+verseny_ido time(7) NOT NULL
+szezon_id char(2) NOT NULL
+futam_id char(2) NOT NULL
+auto_id char(2) NOT NULL
+konstruktor_id char(2) NOT NULL
+CONSTRAINT PK_helyezesek PRIMARY KEY (versenyzo_id ASC)
+ )
+GO
+
+-- CREATE TABLE autok
+CREATE TABLE Formula_1.dbo.autok(
+auto_id char(2) NOT NULL
+nev nvarchar(50) NOT NULL
+karosszeria_tervezo_id char(2) NOT NULL
+motor_tervezo_id char(2) NOT NULL
+teljesitmeny char(4) NOT NULL
+tömeg char(4) NOT NULL
+CONSTRAINT PK_autok PRIMARY KEY (auto_id ASC)
+ )
+GO
+
+-- CREATE TABLE szezon
+CREATE TABLE Formula_1.dbo.szezon(
+szezon_id char(2) NOT NULL
+ev date NOT NULL
+CONSTRAINT PK_szezon PRIMARY KEY (szezon_id ASC)
+ )
+GO
