737ba688c1ab4e0f1306dc661b50259230027d8e
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-10-10 01:05:16 -0700

    Update and rename create_and_fill_f1_tables.sql to create_and_fill_f1_database_and_tables.sql

--- formula_1/create_and_fill_f1_database_and_tables.sql
+++ formula_1/create_and_fill_f1_database_and_tables.sql
@@ -1,3 +1,17 @@
+USE master
+GO
+
+IF  EXISTS (
+	SELECT name 
+		FROM sys.databases 
+		WHERE name = N'Formula_1'
+)
+DROP DATABASE Formula_1
+GO
+
+CREATE DATABASE Formula_1
+GO
+
 USE Formula_1
 GO
 
