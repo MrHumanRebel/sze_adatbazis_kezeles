2cd31e9ea5860990fd4ef855d782fbe9ea0a1a89
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-10-19 11:17:01 -0700

    Create query_top3_legsikeresebb_auto.sql

---
+++ formula_1/query_top3_legsikeresebb_auto.sql
@@ -0,0 +1,7 @@
+/*Listázza ki a 3 legsikeresebb (legtöbb futamot nyert) autómodellt!*/
+USE Formula_1
+SELECT TOP 3 autok.nev,COUNT(helyezesek.elert_helyezes) AS 'db'
+FROM helyezesek INNER JOIN autok
+ON helyezesek.elert_helyezes = 1 AND helyezesek.elert_helyezes != '\N ' AND helyezesek.auto_id = autok.auto_id
+GROUP BY autok.nev
+ORDER BY 2 DESC
