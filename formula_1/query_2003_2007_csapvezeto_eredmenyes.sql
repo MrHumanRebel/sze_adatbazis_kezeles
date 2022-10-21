9528076c66c7f876b7cc70ebfe5e70990b2f3b54
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-10-21 05:26:19 -0700

    Create query_2003_2007_csapvezeto_eredmenyes.sql

---
+++ formula_1/query_2003_2007_csapvezeto_eredmenyes.sql
@@ -0,0 +1,7 @@
+USE Formula_1
+SELECT TOP 1 konstruktorok.csapatvezeto, count(helyezesek.elert_helyezes) AS 'db'
+FROM konstruktorok INNER JOIN helyezesek INNER JOIN szezon
+ON szezon.ev BETWEEN '2003' AND '2007'
+ON helyezesek.elert_helyezes='1' AND helyezesek.elert_helyezes != '\N ' AND helyezesek.konstruktor_id=konstruktorok.konstruktor_id AND szezon.szezon_id = helyezesek.szezon_id
+GROUP BY konstruktorok.csapatvezeto
+ORDER BY 2 DESC
