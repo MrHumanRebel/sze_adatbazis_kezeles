5a0971851fc381fa3dff16422527851fd41858e2
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-10-24 07:24:45 -0700

    fix

--- formula_1/queries/query_napos_hungaroring.sql
+++ formula_1/queries/query_napos_hungaroring.sql
@@ -1,9 +1,8 @@
-/*
-    Többtáblás lekérdezések
-    2. Számítsa ki, hány “napos” időben szervezett futam volt a Hungaroringen eddig?
-*/
+/* 2. Számítsa ki, hány “napos” időben szervezett futam volt a Hungaroringen eddig? */
 
 USE Formula_1
-SELECT helyszinek.nev, COUNT(futamok.idojarasi_korulmeny_id) AS 'db'
-FROM helyszinek INNER JOIN futamok INNER JOIN idojarasi_korulmenyek
-ON futamok.palyanev = "Hungaroring" AND idojarasi_korulmenyek.nev == "napos"
+SELECT palyanev, COUNT(futamok.idojarasi_korulmeny_id) AS 'db'
+FROM helyszin INNER JOIN futamok INNER JOIN idojarasi_korulmenyek
+ON idojarasi_korulmenyek.nev='napos' AND idojarasi_korulmenyek.idojarasi_korulmeny_id = futamok.idojarasi_korulmeny_id
+ON helyszin.palyanev='Hungaroring' AND futamok.helyszin_id = helyszin.helyszin_id
+GROUP BY palyanev
