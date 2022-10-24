87d255ddcf01cbbd5de68ca86dffa64691fcafe6
MrHumanRebel <80947401+MrHumanRebel@users.noreply.github.com>
2022-10-24 08:28:38 -0700

    Create query_nurburgring_20szazad.sql

---
+++ formula_1/queries/query_nurburgring_20szazad.sql
@@ -0,0 +1,11 @@
+/*A Nürburgringen a 20. században hány különböző konstruktőr vonultatta fel a versenyautóit?  */
+
+USE Formula_1
+SELECT helyszin.palyanev, COUNT(konstruktorok.nev) AS 'db'
+FROM szezon INNER JOIN helyezesek INNER JOIN helyszin INNER JOIN futamok INNER JOIN autok INNER JOIN konstruktorok
+ON autok.karosszeria_tervezo_id = konstruktorok.konstruktor_id
+ON autok.auto_id = helyezesek.auto_id
+ON helyszin.palyanev='Nurburgring' AND helyszin.helyszin_id = futamok.helyszin_id
+ON futamok.futam_id = helyezesek.futam_id
+ON (szezon.ev BETWEEN 1900 AND 1999) AND szezon.szezon_id = futamok.szezon_id
+
