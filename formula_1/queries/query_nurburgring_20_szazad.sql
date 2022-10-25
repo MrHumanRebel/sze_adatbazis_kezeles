bc7d629fafc8557c233e2829fe897fdbaae24a4f
Károly Szakály <karoly.szakaly2000@gmail.com>
2022-10-25 17:26:25 +0200

    fix(f1): adjust table names in 2 queries

--- formula_1/queries/query_nurburgring_20_szazad.sql
+++ formula_1/queries/query_nurburgring_20_szazad.sql
@@ -1,7 +1,7 @@
 /*A Nürburgringen a 20. században hány különböző konstruktőr vonultatta fel a versenyautóit?  */
 
 USE Formula_1
-SELECT helyszin.palyanev, COUNT(DISTINCT konstruktorok.nev) AS 'db'
+SELECT helyszinek.palyanev, COUNT(DISTINCT konstruktorok.nev) AS 'db'
 FROM szezonok INNER JOIN helyszinek INNER JOIN futamok INNER JOIN helyezesek INNER JOIN autok INNER JOIN konstruktorok
 ON autok.karosszeria_tervezo_id = konstruktorok.konstruktor_id
 ON autok.auto_id = helyezesek.auto_id
